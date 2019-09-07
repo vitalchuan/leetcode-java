73982dc1897441a5bac10a69d35af673cec1a25a
vitalchuan <610617192@qq.com>
2019-09-07 21:52:50 +0800

    submit 2 finished leetcode problems and a unfinished one.

---
+++ 1.两数之和.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> hMap=new HashMap<>();
+        // HashSet<Integer> hSet=new HashSet<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            int complement=target-nums[i];
+            if(!hMap.containsKey(complement))
+                hMap.put(nums[i], i);
+            else
+                return new int[]{hMap.get(complement),i};
+        }
+        return null;
+    }
+}
+
