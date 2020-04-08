a7ffc7877164bebe0feb0a779cafb2b313e4e5ad
vitalchuan <610617192@qq.com>
2020-04-08 18:05:43 +0800

    fixed problem 37

--- 37.解数独.java
+++ 37.解数独.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 /*
  * @lc app=leetcode.cn id=37 lang=java
  *
@@ -8,99 +7,52 @@ import java.util.*;
 
 // @lc code=start
 class Solution {
+    boolean[][] usedRow = new boolean[9][9];
+    boolean[][] usedColumn = new boolean[9][9];
+    boolean[][] usedBlock = new boolean[9][9];
+
     public void solveSudoku(char[][] board) {
+        for (int row = 0; row < 9; row++)
+            for (int column = 0; column < 9; column++) {
+                if (board[row][column] != '.') {
+                    char ch = board[row][column];
+                    usedRow[row][ch - '0'-1] = true;
+                    usedColumn[column][ch - '0'-1] = true;
+                    usedBlock[row / 3 * 3 + column / 3][ch - '0'-1] = true;
+                }
+            }
         dfs(board, 0, 0);
     }
 
-    public void dfs(char[][] board, int row, int column) {
+    public boolean dfs(char[][] board, int row, int column) {
+        if (column == 9) {
+            row++;
+            column = 0;
+            return dfs(board, row, column);
+        }
+        if (row == 9) {
+            return true;
+        }
         if (board[row][column] == '.') {
-            for (int i = 1; i <= 9; i++) {
-                board[row][column] = (char) (48 + i);
-                if (!isValidSudoku(board))
-                    board[row][column] = '.';
-                else {
-                    
-                    if (row == 8 && column == 8)
-                        return;
-                    if (column == 8)
-                        dfs(board, row + 1, column);
-                    else
-                        dfs(board, row, column + 1);
+            for (char ch = '1'; ch <= '9'; ch++) {
+                board[row][column] = ch;
+                if (!usedRow[row][ch - '0'-1] && !usedColumn[column][ch - '0'-1]
+                        && !usedBlock[row / 3 * 3 + column / 3][ch - '0'-1]) {
+                    usedRow[row][ch - '0'-1] = true;
+                    usedColumn[column][ch - '0'-1] = true;
+                    usedBlock[row / 3 * 3 + column / 3][ch - '0'-1] = true;
+                    if (dfs(board, row, column + 1))
+                        return true;
+                    usedRow[row][ch - '0'-1] = false;
+                    usedColumn[column][ch - '0'-1] = false;
+                    usedBlock[row / 3 * 3 + column / 3][ch - '0'-1] = false;
                 }
+                board[row][column] = '.';
             }
+        } else if (Character.isDigit(board[row][column])) {
+            return dfs(board, row, column + 1);
         }
-        // board[row][column]是数字
-        if (row == 8 && column == 8)
-            return;
-        if (column == 8)
-            dfs(board, row + 1, column);
-        else
-            dfs(board, row, column + 1);
-    }
-
-    public boolean isValidSudoku(char[][] board) {
-        if (!isValidSudokuColumn(board))
-            return false;
-        if (!isValidSudokuRow(board))
-            return false;
-        if (!isValidSudokuBlock(board))
-            return false;
-        return true;
-    }
-
-    /**
-     * 检测行是否有效
-     * 
-     * @param board
-     * @return
-     */
-    public boolean isValidSudokuRow(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (Character.isDigit(board[i][j]))
-                    if (set.contains(board[i][j]))
-                        return false;
-                    else
-                        set.add(board[i][j]);
-            }
-        }
-        return true;
-    }
-
-    /**
-     * 检测列是否有效
-     * 
-     * @param board
-     * @return
-     */
-    public boolean isValidSudokuColumn(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            Set<Character> set = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (Character.isDigit(board[j][i]))
-                    if (set.contains(board[j][i]))
-                        return false;
-                    else
-                        set.add(board[j][i]);
-            }
-        }
-        return true;
-    }
-
-    public boolean isValidSudokuBlock(char[][] board) {
-        for (int i = 0; i < 9; i += 3)
-            for (int j = 0; j < 9; j += 3) {
-                Set<Character> set = new HashSet<>();
-                for (int m = 0; m < 3; m++)
-                    for (int n = 0; n < 3; n++)
-                        if (Character.isDigit(board[i + m][j + n]))
-                            if (set.contains(board[i + m][j + n]))
-                                return false;
-                            else
-                                set.add(board[i + m][j + n]);
-            }
-        return true;
+        return false;
     }
 }
 // @lc code=end
