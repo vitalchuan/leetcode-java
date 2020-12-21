d1d57a7238932be05fd2e864a3e02777edc5535d
vitalchuan <610617192@qq.com>
2020-12-21 21:28:55 +0800

    finished 75

--- 75.颜色分类.java
+++ 75.颜色分类.java
@@ -7,8 +7,35 @@
 // @lc code=start
 class Solution {
     public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for (int i = 0; i < nums.length; i++) {
+            switch (nums[i]) {
+                case 0:
+                    count[0]++;
+                    break;
+                case 1:
+                    count[1]++;
+                    break;
+                case 2:
+                    count[2]++;
+                    break;
+                default:
+                    break;
+            }
+        }
 
+        int index = 0;
+        for (int j = 0; j < count[0]; j++) {
+            nums[index++] = 0;
+        }
+
+        for (int j = 0; j < count[1]; j++) {
+            nums[index++] = 1;
+        }
+
+        for (int j = 0; j < count[2]; j++) {
+            nums[index++] = 2;
+        }
     }
 }
 // @lc code=end
-
